e91516a1be1110a7db15c2c4d33f3bf9c794f573
Felipe Andrade <consultoria.worksbi@worksbi.onmicrosoft.com>
2025-09-09 06:26:15 -0300

    adjust pck.json

--- src/database/reset-db.ts
+++ src/database/reset-db.ts
@@ -4,3 +4,4 @@ import { sql } from 'drizzle-orm'
 await db.execute(sql`DROP SCHEMA public CASCADE`);
 await db.execute(sql`DROP SCHEMA drizzle CASCADE`);
 await db.execute(sql`CREATE SCHEMA public`);
+// "db:reset": "tsx --env-file=.env src/database/reset-db.ts", 
